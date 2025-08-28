eaa75283b8a021a010744ad573c2a55731fa3118
nikhi <nikhilbabu887746@gmail.com>
2025-08-29 01:50:27 +0530

    updated card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -15,11 +15,13 @@ const Card = (props)=>{
 
     }
     return (
-        <div className="flex flex-col w-70  rounded-md shadow-xl items-center pb-4 gap-1  transition-transform duration-300 ease-in-out hover:scale-105  px-5">
+        <div className="relative group overflow-hidden flex flex-col w-70  rounded-md shadow-xl items-center pb-4 gap-1  transition-transform duration-300 ease-in-out hover:scale-105  px-5">
            <img src={props.data.image} alt="" className="h-40 w-35"/>
            <p className="font-semibold text-lg">{(props.data.title.length > 20)? `${props.data.title.slice(0,19)}...` :  `${props.data.title}`}</p>
            <p className="font-semibold text-lg">Price : ${props.data.price}</p>
            <button className="py-2 px-4 bg-orange-400 rounded-md text-gray-200 cursor-pointer mt-[3px]" onClick={()=>handleAddToCart(props.data)}>Add To cart</button>
+           <span className="absolute bottom-0 left-0 h-[3px] w-0 bg-gradient-to-r from-amber-400 via-orange-500 to-amber-400 group-hover:w-full transition-all duration-700 ease-out"></span>
+
         </div>
     )
 }
